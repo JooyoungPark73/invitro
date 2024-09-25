12217d10f86b42660d724317e020a759ce1d4b68
Leonid Kondrashov <leo.kondrashov@gmail.com>
2024-09-25 11:57:51 +0800

    Improve metric collection
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- scripts/setup/expose_infra_metrics.sh
+++ scripts/setup/expose_infra_metrics.sh
@@ -51,7 +51,7 @@ server_exec() {
 	server_exec 'cd loader; kubectl apply -f config/prometh_kn.yaml'
 
 	#* Bind addresses of the control manager and scheduler to "0.0.0.0" so that prometheus can scrape them from any domains.
-	server_exec 'cd loader; sudo kubeadm upgrade apply --config config/kubeadm_init.yaml --ignore-preflight-errors all --force --v=7'
+	server_exec 'cd loader; sudo kubeadm upgrade node --config config/kubeadm_init.yaml --ignore-preflight-errors all --force --v=7'
 
 	sleep 5
 
